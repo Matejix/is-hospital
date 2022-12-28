b78f477c6f62021a48b20be4c2564f9691684f99
sv8520 <ver.karcolova@gmail.com>
2022-12-28 11:53:40 +0100

    hidden reports

--- src/hooks/useCardItems.tsx
+++ src/hooks/useCardItems.tsx
@@ -12,48 +12,48 @@ import {
 function useCardItems() {
   return useMemo(
     () => [
-      // {
-      //   text: "Správa pacientov",
-      //   icon: (
-      //     <UsersIcon className="w-24 h-24 text-slate-700 group-hover:text-slate-50 transition duration-500" />
-      //   ),
-      //   link: "patient-service",
-      // },
-      // {
-      //   text: "Rozpis služieb",
-      //   icon: (
-      //     <CalendarDaysIcon className="w-24 h-24 text-slate-700 group-hover:text-slate-50 transition duration-500" />
-      //   ),
-      //   link: "schedule",
-      // },
-      // {
-      //   text: "Lekarsky predpis",
-      //   icon: (
-      //     <PencilIcon className="w-24 h-24 text-slate-700 group-hover:text-slate-50 transition duration-500" />
-      //   ),
-      //   link: "recipe",
-      // },
-      // {
-      //   text: "Žiadanka",
-      //   icon: (
-      //     <DocumentPlusIcon className="w-24 h-24 text-slate-700 group-hover:text-slate-50 transition duration-500" />
-      //   ),
-      //   link: "requester",
-      // },
-      // {
-      //   text: "Hospitalizacia",
-      //   icon: (
-      //     <PlusCircleIcon className="w-24 h-24 text-slate-700 group-hover:text-slate-50 transition duration-500" />
-      //   ),
-      //   link: "hospitalization",
-      // },
       {
-        text: "Reporty",
+        text: "Správa pacientov",
         icon: (
-          <ClipboardDocumentListIcon className="w-24 h-24 text-slate-700 group-hover:text-slate-50 transition duration-500" />
+          <UsersIcon className="w-24 h-24 text-slate-700 group-hover:text-slate-50 transition duration-500" />
         ),
-        link: "reports",
+        link: "patient-service",
       },
+      {
+        text: "Rozpis služieb",
+        icon: (
+          <CalendarDaysIcon className="w-24 h-24 text-slate-700 group-hover:text-slate-50 transition duration-500" />
+        ),
+        link: "schedule",
+      },
+      {
+        text: "Lekarsky predpis",
+        icon: (
+          <PencilIcon className="w-24 h-24 text-slate-700 group-hover:text-slate-50 transition duration-500" />
+        ),
+        link: "recipe",
+      },
+      {
+        text: "Žiadanka",
+        icon: (
+          <DocumentPlusIcon className="w-24 h-24 text-slate-700 group-hover:text-slate-50 transition duration-500" />
+        ),
+        link: "requester",
+      },
+      {
+        text: "Hospitalizacia",
+        icon: (
+          <PlusCircleIcon className="w-24 h-24 text-slate-700 group-hover:text-slate-50 transition duration-500" />
+        ),
+        link: "hospitalization",
+      },
+      // {
+      //   text: "Reporty",
+      //   icon: (
+      //     <ClipboardDocumentListIcon className="w-24 h-24 text-slate-700 group-hover:text-slate-50 transition duration-500" />
+      //   ),
+      //   link: "reports",
+      // },
       {
         text: "Databázový model",
         icon: (
