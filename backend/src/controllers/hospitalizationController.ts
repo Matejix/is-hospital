a2433a6831331fcc96eb6b60e84f1341d9de87b8
grilusova <92672993+grilusova@users.noreply.github.com>
2023-01-03 16:13:15 +0100

    Hospitalizacia

---
+++ backend/src/controllers/hospitalizationController.ts
@@ -0,0 +1,45 @@
+import getDBConnection from "../database";
+import express, { Request, Response } from "express";
+const hospitalizationController = express.Router();
+
+hospitalizationController.get("/getPatients", async (req: Request, res: Response) => {
+    const connection = await getDBConnection();
+
+    const query = await connection?.execute(
+      `select meno, priezvisko, rod_cislo, ulica, nazov_mesta, nazov from is_pacient join is_osoba using (rod_cislo)
+       join is_mesto using (psc) join is_poistenia using(rod_cislo) join is_poistovna using(id_poistovna) fetch first 1000 rows only`
+    );
+    var rows = JSON.parse(JSON.stringify(query?.rows));
+    res.json(rows);
+});
+
+hospitalizationController.get("/getDepartments", async (req: Request, res: Response) => {
+  const connection = await getDBConnection();
+
+  const query = await connection?.execute(
+    `select o.informacie_oddelenia.nazov_oddelenia as "NAZOV", o.informacie_oddelenia.popis_oddelenia as "POPIS" 
+     from is_typ_oddelenia o fetch first 1000 rows only`
+  );
+  var rows = JSON.parse(JSON.stringify(query?.rows));
+  res.json(rows);
+});
+
+
+/*hospitalizationRouter.post("/", async (req: Request, res: Response) => {
+  const { description, id_employee, id_patient, medicine } = req.body;
+  console.log(description, id_employee, id_patient, medicine);
+
+  const connection = await getDBConnection();
+  const query = await connection?.execute(
+    `insert into is_zaznam (id_zaznam,datum_zaznamu,popis,id_sprava,id_zamestnanec) values ((select max(id_zaznam) + 1 from id_zaznam), '${description}', '${id_patient}', ${id_employee})
+     join is_zdravotna_karta usingwhere id_pacient = `
+  );
+  console.log(query);
+  medicine.map((medicament: any) => {
+    connection?.execute(
+      `insert into is_predpisane_lieky (id_predpisu,id_kod_lieku) values ((select max(id_predpisu) from is_predpis), '${medicament}')`
+    );
+  });
+});*/
+
+export default hospitalizationController;
---
+++ src/types.ts
@@ -0,0 +1,29 @@
+export type LogUser = {
+    password: string;
+    id_employee: number;
+    username: string;
+    exp: number;
+    iat: number;
+  };
+  
+  export interface MedicineData {
+    ID_KOD_LIEKU: number;
+    NAZOV: string;
+  }
+
+  export interface DepartmentsData{
+    NAZOV: string;
+    POPIS: string;
+  }
+  
+  export interface PatientsData {
+    MENO: string;
+    PRIEZVISKO: string;
+    ROD_CISLO: string;
+    ULICA: string;
+    NAZOV_MESTA: string;
+    NAZOV: string; // poistovna
+  }
+  export interface TableSortProps {
+    data: PatientsData[];
+  }
