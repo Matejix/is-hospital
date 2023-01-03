a0880d8cfd9b6094bc507302a4123689b79452c4
sv8520 <ver.karcolova@gmail.com>
2023-01-03 15:26:27 +0100

    Added records of patients to patients service

--- backend/src/controllers/patientServiceController.ts
+++ backend/src/controllers/patientServiceController.ts
@@ -6,10 +6,110 @@ patientServiceRouter.get("/getPatients", async (req: Request, res: Response) => 
     const connection = await getDBConnection();
 
     const query = await connection?.execute(
-      `select meno, priezvisko, rod_cislo from is_pacient join is_osoba using (rod_cislo) fetch first 100 rows only`
+      `select  priezvisko || ' ' || meno as cele_meno, rod_cislo from is_pacient join is_osoba using (rod_cislo) fetch first 100 rows only`
     );
     var rows = JSON.parse(JSON.stringify(query?.rows));
     res.json(rows);
+});
+
+
+patientServiceRouter.post("/getBasicInfo", async (req: Request, res: Response) => {
+  const {id} = req.body;
+  const connection = await getDBConnection();
+
+  const query = await connection?.execute(
+    `select rod_cislo, titul, meno, priezvisko, rodne_priezvisko, ulica, psc, nazov_mesta, f_datum_narodenia(rod_cislo) as datum_narodenia, datum_umrtia, krvna_skupina, zamestnanie, nazov as poistovna from is_pacient
+    join is_osoba using (rod_cislo)
+    join is_mesto using (psc)
+    join is_poistenia using (rod_cislo)
+    join is_poistovna using (id_poistovna)
+    where dat_do is null and
+     rod_cislo = '${id}'`
+  );
+  var rows = JSON.parse(JSON.stringify(query?.rows));
+  res.json(rows);
+});
+
+
+patientServiceRouter.post("/getListOfRecords", async (req: Request, res: Response) => {
+  const {id} = req.body;
+  const connection = await getDBConnection();
+
+  const query = await connection?.execute(
+    `select id_zaznam, datum_zaznamu, case when id_alergia is not null then 'Alergia' 
+    when id_vykon is not null then 'Výkon'  
+    when id_sprava is not null then 'Správa'  
+    when id_vysetrenie is not null then 'Vyšetrenie'  
+    when id_medikacia is not null then 'Medikácia'  
+    end as zaznam,
+    case when id_alergia is not null then  nazov_alergie 
+    when id_vykon is not null then nazov_vykonu 
+    when id_sprava is not null then  typ_spravy 
+    when id_vysetrenie is not null then nazov_vysetrenia
+    when id_medikacia is not null then nazov_medikacie 
+    end as typ,
+    is_zaznamy.popis, davkovanie_medikacie, diagn.nazov as nazov_diagnozy,
+    titul || ' ' || meno ||  ' ' || priezvisko as vystavil
+      from is_zaznamy 
+      left join is_alergie using (id_alergia) 
+      left join is_vykony using (id_vykon) 
+      left join is_sprava using (id_sprava) 
+      left join is_medikacie using (id_medikacia) 
+      left join is_vysetrenie using (id_vysetrenie) 
+      left join is_diagnoza diagn using (kod_diagnozy)
+      left join is_zamestnanec using (id_zamestnanec)
+      left join is_osoba on (is_zamestnanec.rod_cislo = is_osoba.rod_cislo)
+ where is_zaznamy.rod_cislo = '${id}'
+ order by datum_zaznamu
+`
+  );
+  var rows = JSON.parse(JSON.stringify(query?.rows));
+  res.json(rows);
+});
+
+
+patientServiceRouter.post("/getRequests", async (req: Request, res: Response) => {
+  const {id} = req.body;
+  const connection = await getDBConnection();
+
+  const query = await connection?.execute(
+    `select id_ziadanky, dat_vystavenia, is_ziadanky.popis, diagn.nazov as nazov_diagnozy,
+    zam1.titul || ' ' || zam1.meno ||  ' ' || zam1.priezvisko as vystavil,
+    x.informacie_oddelenia.nazov_oddelenia as oddelenie
+from is_ziadanky 
+left join is_diagnoza diagn using (kod_diagnozy)
+join is_zamestnanec on (is_ziadanky.id_zamestnanec = is_zamestnanec.id_zamestnanec)
+join is_osoba zam1 on (is_zamestnanec.rod_cislo = zam1.rod_cislo)
+left join is_typ_oddelenia x using (id_typu_oddelenia)
+ where is_ziadanky.rod_cislo = '${id}'
+ order by dat_vystavenia
+`
+  );
+  var data = query?.rows
+  var rows = JSON.parse(JSON.stringify(query?.rows));
+  res.json(rows);
+});
+
+patientServiceRouter.post("/getPrescriptions", async (req: Request, res: Response) => {
+  const {id} = req.body;
+  const connection = await getDBConnection();
+
+  const query = await connection?.execute(
+    `select id_predpisu || id_kod_lieku as kod, popis,
+    zam1.titul || ' ' || zam1.meno ||  ' ' || zam1.priezvisko as vystavil, 
+    x.liek.nazov as liek
+     from is_predpis 
+    join is_predpisane_lieky using (id_predpisu)
+     join is_lieky x using (id_kod_lieku)
+    
+    join is_zamestnanec on (is_predpis.id_zamestnanec = is_zamestnanec.id_zamestnanec)
+    join is_osoba zam1 on (is_zamestnanec.rod_cislo = zam1.rod_cislo)
+ where is_predpis.rod_cislo = '${id}'
+`
+  );
+  var data = query?.rows
+  var rows = JSON.parse(JSON.stringify(query?.rows));
+  res.json(rows);
 });
 
 export default patientServiceRouter;
--- src/pages/PatientService/PatientService.tsx
+++ src/pages/PatientService/PatientService.tsx
@@ -10,7 +10,7 @@ import {
   Center,
   TextInput,
   Menu,
-  ActionIcon,
+  ActionIcon, Accordion, Tabs 
 } from "@mantine/core";
 import { keys } from "@mantine/utils";
 import {
@@ -25,6 +25,7 @@ import {
   IconDots,
 } from "@tabler/icons";
 import axios from "axios";
+import internal from "stream";
 
 const useStyles = createStyles((theme) => ({
   th: {
@@ -48,13 +49,67 @@ const useStyles = createStyles((theme) => ({
     height: 21,
     borderRadius: 21,
   },
+  wrapper: {
+    paddingTop: theme.spacing.xl * 2,
+    paddingBottom: theme.spacing.xl * 2,
+    minHeight: 650,
+  },
+
+  item: {
+    backgroundColor: "#ffffff",
+
+    borderbottom: `1px solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+  },
 }));
 
 interface RowData {
-  MENO: string;
-  PRIEZVISKO: string;
+  CELE_MENO: string;
   ROD_CISLO: string;
 }
+interface BasicInfo {
+  ROD_CISLO: string;
+  TITUL: string;
+  MENO: string;
+  PRIEZVISKO: string;
+  RODNE_PRIEZVISKO: string;
+  ULICA: string;
+  PSC: string;
+  NAZOV_MESTA: string;
+  DATUM_NARODENIA: Date;
+  DATUM_UMRTIA: Date;
+  KRVNA_SKUPINA: string;
+  ZAMESTNANIE: string; 
+  POISTOVNA: string;
+}
+
+interface Records {
+  ID_ZAZNAM: string;
+  DATUM_ZAZNAMU: Date;
+  ZAZNAM: string;
+  TYP: string;
+  POPIS: string;
+  DAVKOVANIE_MEDIKACIE: string;
+  NAZOV_DIAGNOZY: string;
+  VYSTAVIL: string;
+}
+
+interface Requests {
+  ID_ZIADANKY: string;
+  DAT_VYSTAVENIA: Date;
+  POPIS: string;
+  NAZOV_DIAGNOZY: string;
+  VYSTAVIL: string;
+  ODDELENIE: string;
+}  
+
+interface Prescriptions {
+  KOD: string;
+  VYSTAVIL: string;
+  LIEK: string;
+}  
+
 interface TableSortProps {
   data: RowData[];
 }
@@ -86,7 +141,6 @@ function Th({ children, reversed, sorted, onSort }: ThProps) {
     </th>
   );
 }
-
 
 
 function filterData(data: RowData[], search: string) {
@@ -124,30 +178,100 @@ function MainBlockComponent() {
 }
 
 function PatientService() {
+  const { classes } = useStyles();
+
   useEffect(() => {
     getUsers();
   }, []);
   const [patients, setPatients] = useState<RowData[] | null>(null);
-  const getUsers = () => {
-    axios
-      .get("http://localhost:3000/patientservice/getPatients")
-      .then((response: any) => {
-        console.log(response);
-        setPatients(response.data);
-      });
-  };
+  const [basicInfo, setBasicInfo] = useState<BasicInfo | null>(null);
+  const [patientRecords, setPatientRecords] = useState<Records[] | null>(null);
+  const [patientRequests, setPatientRequests] = useState<Requests[] | null>(null);
+  const [patientPrescriptions, setPatientPrescriptions] = useState<Prescriptions[] | null>(null);
 
-  const [choosedPerson, setChoosedPerson] = useState(false);
+  const [choosenPatient, setChoosenPatient] = useState("");
   const [search, setSearch] = useState("");
   const [sortedData, setSortedData] = useState(patients);
   const [sortBy, setSortBy] = useState<keyof RowData | null>(null);
   const [reverseSortDirection, setReverseSortDirection] = useState(false);
 
+  const getUsers = () => {
+    axios
+      .get("http://localhost:3000/patientservice/getPatients")
+      .then((response: any) => {
+        setPatients(response.data);
+        setChoosenPatient(response.data[0].ROD_CISLO);
+      });
+  };
+
+  const getPatientInfo = () => {
+    axios.post("http://localhost:3000/patientservice/getBasicInfo", {
+      id: choosenPatient
+    }).then((response: any) => {
+        setBasicInfo(response.data[0]);
+    });
+  };
+  
+  const getPatientRecords = () => {
+    axios.post("http://localhost:3000/patientservice/getListOfRecords", {
+      id: choosenPatient
+    }).then((response: any) => {
+        setPatientRecords(response.data);
+    });
+  };
+
+  const getPatientRequests = () => {
+    axios.post("http://localhost:3000/patientservice/getRequests", {
+      id: choosenPatient
+    }).then((response: any) => {
+        setPatientRequests(response.data);
+    });
+  };
+
+  const getPatientPrescriptions = () => {
+    axios.post("http://localhost:3000/patientservice/getPrescriptions", {
+      id: choosenPatient
+    }).then((response: any) => {
+        setPatientPrescriptions(response.data);
+    });
+  };
+
+
+
   useEffect(() => {
     if (patients !== null && sortedData === null && sortBy === null) {
       setSortedData(patients);
+      setChoosenPatient(patients[0].ROD_CISLO);
+      getPatientInfo();
+      getPatientRecords();
+      getPatientRequests();
+      getPatientPrescriptions();
     }
   }, [patients]);
+
+  useEffect(() => {
+    if (patients !== null ) {
+      getPatientInfo();
+    }
+  }, [choosenPatient]);
+
+  useEffect(() => {
+    getPatientInfo();
+  }, [basicInfo])
+
+  useEffect(() => {
+    getPatientRecords();
+  }, [patientRecords])
+
+  useEffect(() => {
+    getPatientRequests();
+  }, [patientRequests])
+
+  useEffect(() => {
+    getPatientPrescriptions();
+  }, [patientPrescriptions])
+
+
 
   const setSorting = (field: keyof RowData) => {
     const reversed = field === sortBy ? !reverseSortDirection : false;
@@ -171,16 +295,13 @@ function PatientService() {
   };
 
   const choosePerson = (row : RowData) => {
-    console.log(choosedPerson);
-    alert(row.MENO + " " +row.PRIEZVISKO);
-    setChoosedPerson(true);
-    console.log(choosedPerson);
+    setChoosenPatient(row.ROD_CISLO);
+    getPatientInfo();
   };
   
   const rows = sortedData?.map((row: any) => (
-    <tr key={`${row.MENO}-${row.PRIEZVISKO}-${row.ROD_CISLO}`} >
-      <td onClick={() => choosePerson(row)}>{row.MENO}</td>
-      <td onClick={() => choosePerson(row)}>{row.PRIEZVISKO}</td>
+    <tr key={`${row.CELE_MENO}-${row.ROD_CISLO}`} >
+      <td onClick={() => choosePerson(row)}>{row.CELE_MENO}</td>
       <td onClick={() => choosePerson(row)}>{row.ROD_CISLO}</td>
       <td><Menu transition="pop" withArrow position="bottom-end">
             <Menu.Target>
@@ -192,19 +313,72 @@ function PatientService() {
               <Menu.Item icon={<IconReportAnalytics size={16} stroke={1.5} />}>Pridať záznam</Menu.Item>
               <Menu.Item icon={<IconNote size={16} stroke={1.5} />}>Pridať predpis</Menu.Item>
               <Menu.Item icon={<IconTrash size={16} stroke={1.5} />} color="red">
-                Delete patient
+                Zmazať pacienta
               </Menu.Item>
             </Menu.Dropdown>
           </Menu></td>
     </tr>
   ));
-  
+
+  const listOfRecords = patientRecords?.map((row: Records) => (
+    <Accordion >
+        <Accordion.Item className={classes.item} value={row.ID_ZAZNAM.toString()} >
+        <Accordion.Control>
+        <div className="flex justify-between	 ">
+            <a >  {new Date(row.DATUM_ZAZNAMU).toLocaleDateString()} </a> 
+            <a className="text-cyan-900	">           {row.ZAZNAM}</a>
+          </div>
+          </Accordion.Control>
+        <Accordion.Panel className="text-sm">
+          <p className="font-medium"> {row.TYP} </p>
+          <p> {row.POPIS}</p>
+          <p> {row.DAVKOVANIE_MEDIKACIE ? 'Dávkovanie medikácie: ' + row.DAVKOVANIE_MEDIKACIE : ''}</p>
+          <p> {row.NAZOV_DIAGNOZY ? 'Diagnóza: ' + row.NAZOV_DIAGNOZY : ''}</p>
+          <p> {row.VYSTAVIL ? 'Vystavil: ' + row.VYSTAVIL : ''}</p>
+        </Accordion.Panel>
+      </Accordion.Item>
+           
+    </Accordion>
+  ));
+
+  const listOfRequests = patientRequests?.map((row: Requests) => (
+    <Accordion >
+        <Accordion.Item className={classes.item} value={row.ID_ZIADANKY.toString()} >
+        <Accordion.Control>
+        <div className="flex justify-between	 ">
+            <a >  {new Date(row.DAT_VYSTAVENIA).toLocaleDateString()} </a> 
+            <a className="text-cyan-900	">           {row.ODDELENIE}</a>
+          </div>
+          </Accordion.Control>
+        <Accordion.Panel className="text-sm">
+          <p> {row.POPIS}</p>
+          <p> {row.NAZOV_DIAGNOZY ? 'Diagnóza: ' + row.NAZOV_DIAGNOZY : ''}</p>
+          <p> {row.VYSTAVIL ? 'Vystavil: ' + row.VYSTAVIL : ''}</p>
+        </Accordion.Panel>
+      </Accordion.Item>
+           
+    </Accordion>
+  ));
+  const listOfPrescriptions = patientPrescriptions?.map((row: Prescriptions) => (
+    <Accordion >
+        <Accordion.Item className={classes.item} value={row.KOD.toString()} >
+        <Accordion.Control>
+        <a className="text-cyan-900	">{row.LIEK} </a> 
+          </Accordion.Control>
+        <Accordion.Panel className="text-sm">
+          <p> {row.VYSTAVIL ? 'Vystavil: ' + row.VYSTAVIL : ''}</p>
+        </Accordion.Panel>
+      </Accordion.Item>
+           
+    </Accordion>
+  ));
+
   return (
     <div className="PatientsInfo">
       <div className="TableBlock">
         <ScrollArea>
           <TextInput
-            placeholder="Search by any field"
+            placeholder="Prehľadávať"
             mb="md"
             icon={<IconSearch size={14} stroke={1.5} />}
             value={search}
@@ -214,39 +388,33 @@ function PatientService() {
             horizontalSpacing="xs"
             verticalSpacing="xs"
             highlightOnHover
-            sx={{ tableLayout: "fixed", minWidth: 200 }}
+            sx={{ tableLayout: "fixed", minWidth: 150 }}
           >
             <thead >
               <tr >
-                <Th 
-                  sorted={sortBy === "MENO"}
-                  reversed={reverseSortDirection}
-                  onSort={() => setSorting("MENO")}
                
-                >
-                  Name
-                </Th>
                 <Th
-                  sorted={sortBy === "PRIEZVISKO"}
+                  sorted={sortBy === "CELE_MENO"}
                   reversed={reverseSortDirection}
-                  onSort={() => setSorting("PRIEZVISKO")}
+                  onSort={() => setSorting("CELE_MENO")}
                 >
-                  LastName
+                   Meno
                 </Th>
+              
                 <Th
                   sorted={sortBy === "ROD_CISLO"}
                   reversed={reverseSortDirection}
                   onSort={() => setSorting("ROD_CISLO")}
                 >
-                  ID Number
+                  Rodné číslo
                 </Th>
                 <td  className="rowHeader">
                   <UnstyledButton >
-                    <Group position="apart">
-                       <Text weight={500} size="sm">
-                          Edit
+                    
+                       <Text weight={500} size="xs">
+                          Možnosti
                       </Text>
-                    </Group>
+                    
                   </UnstyledButton>
                 </td>
               </tr>
@@ -262,7 +430,7 @@ function PatientService() {
                     }
                     >
                     <Text weight={500} align="center">
-                      Data is loading...
+                      Načítanie...
                     </Text>
                   </td>
                 </tr>
@@ -272,9 +440,69 @@ function PatientService() {
         </ScrollArea>
       </div>
       <div className="MoreInfo">
-    
-      </div>
+       <div className="grid h-screen place-items-center">
+        <div className="CardInfo drop-shadow-lg bg-white    fixed">
+          <div className="py-3 px-6 bg-slate-100	 border-b border-gray-300 flex justify-between	 ">
+            <a className=" font-semibold"> Zdravotná karta </a> 
+            <a className="text-cyan-900	"> {basicInfo?.ROD_CISLO}</a>
+          </div>
+          <div className="py-2 px-6 border-b border-gray-300 flex flex-row">
+            <div >
+            <h5 className="font-bold text-sm pb-1  text-sky-800 ">Poisťovňa: </h5>
 
+              <h5 className="font-bold text-sm pb-1 text-gray-500 ">Meno: </h5>
+              <h5 className="font-bold text-sm pb-1  text-gray-500 ">Priezvisko: </h5>
+              <h5 className="font-bold text-sm pb-1  text-gray-500 ">Rodné priezvisko: </h5>
+              <h5 className="font-bold text-sm pb-1  text-gray-500 ">Dátum narodenia: </h5>
+              <h5 className="font-bold text-sm pb-1  text-gray-500 ">Dátum úmrtia: </h5>
+              <h5 className="font-bold text-sm pb-1  text-gray-500 ">Krvná skupina: </h5>
+              <h5 className="font-bold text-sm pb-1  text-gray-500 ">Adresa: </h5>
+              <h5 className="font-bold text-sm pb-1  text-gray-500 ">Zamestnanie: </h5>
+
+            </div>
+            <div className="text-sm pl-10">
+              <h5 className="pb-1 text-sky-800 font-semibold"> {basicInfo?.POISTOVNA ? basicInfo.POISTOVNA : "-"} </h5>
+
+              <h5 className="pb-1"> {basicInfo?.MENO ? basicInfo.MENO : "-" } </h5>
+              <h5 className="pb-1"> {basicInfo?.PRIEZVISKO ? basicInfo.PRIEZVISKO : "-"} </h5>
+              <h5 className="pb-1 text-gray-500"> {basicInfo?.RODNE_PRIEZVISKO ? basicInfo.PRIEZVISKO : "-"} </h5>
+              <h5 className="pb-1"> {basicInfo?.DATUM_NARODENIA ?  new Date(basicInfo.DATUM_NARODENIA).toLocaleDateString() : "-"}</h5>
+              <h5 className="pb-1 text-gray-500"> {basicInfo?.DATUM_UMRTIA ?  new Date(basicInfo.DATUM_NARODENIA).toLocaleDateString() : "-"}</h5>
+              <h5 className="pb-1"> {basicInfo?.KRVNA_SKUPINA} </h5>
+              <h5 className="pb-1"> {basicInfo?.ULICA ? basicInfo.ULICA : "-"} {basicInfo?.NAZOV_MESTA} {basicInfo?.PSC}  </h5>
+              <h5 className="pb-1"> {basicInfo?.ZAMESTNANIE ? basicInfo.ZAMESTNANIE : "-"} </h5>
+            </div>
+          </div>
+          <div >
+
+          <Tabs  defaultValue="Records">
+              <Tabs.List className=" px-6 bg-slate-100	  ">
+                <Tabs.Tab value="Records">Záznamy</Tabs.Tab>
+                <Tabs.Tab value="Requests">Žiadanky</Tabs.Tab>
+                <Tabs.Tab value="Prescription">Predpisy</Tabs.Tab>
+              </Tabs.List>
+
+              <Tabs.Panel value="Records" pt="xs">
+              <div className="records overflow-y-auto"          >
+                {listOfRecords}
+             </div>
+              </Tabs.Panel>
+
+              <Tabs.Panel value="Requests" pt="xs">
+                {listOfRequests}
+              </Tabs.Panel>
+
+              <Tabs.Panel value="Prescription" pt="xs">
+                {listOfPrescriptions}
+              </Tabs.Panel>
+            </Tabs>
+
+
+          </div>
+          
+        </div>
+      </div>
+      </div>
     </div>
   );
 }
